9e8970407212387d02207f165551539cfda2fb5c
Jakob Troidl <jakob.troidl@googlemail.com>
2020-06-06 12:24:43 -0400

    setting up two column structure correctly

--- src/treemodel.cpp
+++ src/treemodel.cpp
@@ -30,23 +30,44 @@ void TreeModel::init(QTreeView* tree, DataContainer* datacontainer)
     {
       QString name(object_p->getName().c_str());
       QStandardItem* item_name = new QStandardItem(name);
+      
       int id = object_p->getHVGXID();
       QStandardItem* item_id = new QStandardItem(QString::number(id));
       
+      paramList.appendRow(QList<QStandardItem*>() << item_name << item_id);
 
-      /*std::vector<Object*> children = object_p->getChildren();
+      std::vector<Object*> children = object_p->getChildren();
       int j = 0;
       for (Object* child : children)
       {
+
         QString child_name(child->getName().c_str());
-        QStandardItem* item1 = new QStandardItem(child_name);
-        itemroot->setChild(j, item1);
+        QStandardItem* item_child_name = new QStandardItem(child_name);
+        //item_name->setChild(j, item_child_name);
+
+        int child_id = child->getHVGXID();
+        QStandardItem* child_item_id = new QStandardItem(QString::number(child_id));
+        //item_id->setChild(j, child_item_id);
+
+        item_name->appendRow(QList<QStandardItem*>() << item_child_name << child_item_id);
+
         j++;
-      }*/
-      paramList.setItem(i, 0, item_name);
-      paramList.setItem(i, 1, item_id);
+      }
+      //paramList.setItem(i, 0, item_name);
+      //paramList.setItem(i, 1, item_id);
       i++;
     }
+
+    //QStandardItemModel model;
+    //QStandardItem* parent0 = new QStandardItem("Parent 0");
+    //QStandardItem* col1 = new QStandardItem("1");
+    //model.appendRow(QList<QStandardItem*>() << parent0 << col1);
+    //// make some children                                                                                                          
+    //for (int i = 0; i < 3; ++i) {
+    //  QStandardItem* child0 = new QStandardItem(QString("Child %1").arg(i));
+    //  QStandardItem* child1 = new QStandardItem(QString("%1").arg(i));
+    //  parent0->appendRow(QList<QStandardItem*>() << child0 << child1);
+    //}
     
   }
 }
