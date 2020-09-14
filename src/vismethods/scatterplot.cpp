f3cf47adec9d3c61f49ae7bfc7aeff7a3fbbdb2e
jakob.troidl <jakob.troidl@gmail.com>
2020-09-14 16:09:31 +0200

    scatterplot first implementation

--- src/vismethods/scatterplot.cpp
+++ src/vismethods/scatterplot.cpp
@@ -1,12 +1,19 @@
 #include "scatterplot.h"
 
-ScatterplotData::ScatterplotData(int ID, GlobalVisParameters* m_global_vis_parameters, DataContainer* data_container)
+ScatterplotData::ScatterplotData(QString json, GlobalVisParameters* global_vis_parameters, DataContainer* data_container)
 {
-
+  m_json_string = json;
+  m_global_vis_parameters = global_vis_parameters;
+  m_datacontainer = data_container;
 }
 
 ScatterplotData::~ScatterplotData()
 {
+}
+
+Q_INVOKABLE QString ScatterplotData::getData()
+{
+  return Q_INVOKABLE m_json_string;
 }
 
 Scatterplot::Scatterplot(Scatterplot* scatterplot)
@@ -25,15 +32,52 @@ Scatterplot::~Scatterplot()
 {
 }
 
+QString Scatterplot::createJSONString(QList<int>* selectedObjects)
+{
+  QJsonArray document;
+  for (auto i : *selectedObjects)
+  {
+    QJsonObject object_json;
+
+    Object* object = m_datacontainer->getObjectsMapPtr()->at(i);
+    std::vector<int>* mito_indices = object->get_indices_list();
+    std::vector<VertexData>* vertices = m_datacontainer->getMesh()->getVerticesList();
+
+    double sum = 0.0;
+    double counter = 0.0;
+    double threshold = 0.1;
+    for (auto j : *mito_indices)
+    {
+      VertexData vertex = vertices->at(j);
+      sum += vertex.distance_to_cell;
+
+      if (vertex.distance_to_cell <= threshold)
+      {
+        counter += 1.0;
+      }
+    }
+    double avg = sum / (double)mito_indices->size();
+    double perc = counter / (double)mito_indices->size();
+
+    object_json.insert("avg", avg);
+    object_json.insert("perc", perc);
+
+    document.push_back(object_json);
+  }
+
+  QJsonDocument doc(document);
+  return doc.toJson(QJsonDocument::Indented);
+}
+
 QWebEngineView* Scatterplot::initVisWidget(int ID, SpecificVisParameters params)
 {
-  //QString json = createJSONString(&m_global_vis_parameters->selected_objects, m_global_vis_parameters->distance_threshold);
-  //data = new BarChartData(json, m_datacontainer, m_global_vis_parameters);
+  QString json = createJSONString(&m_global_vis_parameters->selected_objects);
+  m_data = new ScatterplotData(json, m_global_vis_parameters, m_datacontainer);
 
   m_web_engine_view = new QWebEngineView();
   QWebChannel* channel = new QWebChannel(m_web_engine_view->page());
   m_web_engine_view->page()->setWebChannel(channel);
-  //channel->registerObject(QStringLiteral("barchart_data"), data);
+  channel->registerObject(QStringLiteral("scatterplot_data"), m_data);
   m_web_engine_view->load(getHTMLPath(m_index_filename));
 
   return m_web_engine_view;
@@ -46,7 +90,9 @@ QWebEngineView* Scatterplot::getWebEngineView()
 
 bool Scatterplot::update()
 {
-  return false;
+  QString json = createJSONString(&m_global_vis_parameters->selected_objects);
+  m_data->setJSONString(json);
+  return true;
 }
 
 Scatterplot* Scatterplot::clone()
