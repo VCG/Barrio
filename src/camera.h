69490d0f11ac8aa00eb53430f4e225f102911b7d
Jakob Troidl <jakob.troidl@gmail.com>
2021-10-19 13:49:47 -0400

    fix camera bug

--- src/camera.h
+++ src/camera.h
@@ -2,59 +2,62 @@
 
 #include <QVector3D>
 #include <QMatrix4x4>
+#include <QPoint>
+#include <QMouseEvent>
+
 #include <qmath.h>
 #include <math.h>
 
 #include "globalParameters.h"
 
+
 class Camera
 {
-	public:
-		Camera(float fov, float near, float far, QVector3D center);
-		~Camera();
+public:
+	Camera(float fov, float near, float far, QVector3D center);
+	~Camera();
 
-		void frameUpdate();
-		void setPosition(float x, float y, float z);
-		void setPosition(const QVector3D& pos);
-		void setLookAt(const QVector3D& lookAt);
-		void setFront(const QVector3D& newFront);
-		void switchMovement();
-		void setAspectRatio(qreal aspect_ratio);
+	void frameUpdate();
+	void setAspectRatio(qreal aspect_ratio);
 
-		const QVector3D& getPosition() const;
-		const QVector3D& getFront() const;
-		const QVector3D& getUp() const;
-		const QMatrix4x4& getViewMatrix() const;
-		const QMatrix4x4& getProjection() const;
-		const QMatrix4x4& getVP() const;
-		const float& getYaw() const;
-		const float& getPitch() const;
+	QVector3D getPosition();
 
-		void processMouseInput(float deltaX, float deltaY);
-		void processScroll(int delta);
-		void resetCamera();
+	QMatrix4x4& getModelMatrix();
+	QMatrix4x4& getViewMatrix();
+	QMatrix4x4& getProjectionMatrix();
 
-	private:
+	void resetCamera();
+	void mouse_press_event(QMouseEvent* event);
+	void mouse_move_event(QMouseEvent* event);
+	void processScroll(double delta);
 
-		QMatrix4x4 viewMatrix;
-		QMatrix4x4 projection;
+	static int normalize_angle(int angle);
 
-		QVector3D center;
-		QVector3D position;
-		QVector3D front;
-		QVector3D up;
+	void setXRotation(int angle);
+	void setYRotation(int angle);
+	void setZRotation(int angle);
 
-		float fov;
+private:
 
-		float speed;
+	QMatrix4x4 model;
+	QMatrix4x4 view;
+	QMatrix4x4 projection;
 
-		float yaw;
-		float pitch;
-		float distance;
-		float nearPlane;
-        float farPlane;
+	QVector3D center;
+	QVector3D position;
+	QVector3D front;
+	QVector3D up;
 
-		void calculateFront();
+	QPoint last_mouse_position;
 
+	int x_rotation;
+	int y_rotation;
+	int z_rotation;
+
+	float fov;
+	float speed;
+	float distance;
+	float nearPlane;
+	float farPlane;
 };
 
